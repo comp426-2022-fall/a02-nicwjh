92585d752720f086f6249f45739c2f50a4236fb4
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-06 16:25:31 -0500

    first push

--- cli.js
+++ cli.js
@@ -1,1 +1,22 @@
 #!/usr/bin/env node
+
+Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.
+
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+const timezone = moment.tz.guest()
+
+let lattitude = '35.90'
+let longitude = '-79.05' 
+
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
+
+const data = await.response.json();
+
+console.log(data);
