391ed533767b64d408e888f69cd5a35370ff329c
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-06 18:49:16 -0500

    fixed longitude and latitude

--- cli.js
+++ cli.js
@@ -26,8 +26,10 @@ process.exit(0)
 }
 
 console.log(args.n)
-let latitude = args.n
-let longitude = args.w 
+let latitude = args.n||args.s
+let longitude = args.w||args.e
+console.log(latitude)
+console.log(longitude)
 
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
 
