a89a50f9c2282c81d04b819228bc7f8f4c9f134b
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-06 16:37:33 -0500

    fixed latitude

--- cli.js
+++ cli.js
@@ -12,7 +12,7 @@ import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 const timezone = moment.tz.guest()
 
-let lattitude = '35.90'
+let latitude = '35.90'
 let longitude = '-79.05' 
 
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
