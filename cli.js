84ec3ddff490f850cacbbf7cd65cb96618cf6b61
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-06 16:33:48 -0500

    test

--- cli.js
+++ cli.js
@@ -17,7 +17,7 @@ let longitude = '-79.05'
 
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
 
-const data = await.response.json();
+const data = await response.json();
 
 console.log(data);
 
